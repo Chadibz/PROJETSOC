fd256dfc375140f476d6705e1c970b70f51e0a01
Chadibz <91723882+Chadibz@users.noreply.github.com>
2024-01-12 14:33:04 +0100

    first commit

---
+++ src/app/components/traffic-info/info-card.component.ts
@@ -0,0 +1,64 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ShowMapMessage } from 'src/app/messages/show-map.message';
+import { GeoPosition } from 'src/app/view-models/geo-position';
+import { MessageBrokerService } from 'src/app/services/message-broker.service';
+import { TrafficMessageViewModel } from 'src/app/view-models/traffic-message-vm';
+import { TranslationService } from 'src/app/services/translation.service';
+
+@Component({
+    selector: 'app-info-card',
+    templateUrl: './info-card.component.html',
+    styleUrls: ['./info-card.component.scss']
+})
+export class InfoCardComponent implements OnInit {
+    @Input() message: TrafficMessageViewModel;
+    @Input() showMapLink = false;
+    @Input() userPos: GeoPosition;
+
+    constructor(
+        private readonly broker: MessageBrokerService,
+        private readonly translationService: TranslationService
+    ) {}
+
+    ngOnInit(): void {}
+
+    onClickMessage(message: TrafficMessageViewModel) {
+        const pos: GeoPosition = {
+            lat: message.latitude,
+            lng: message.longitude,
+            info: `${message.priorityName}: ${message.title} (${message.subCategory})`,
+            id: message.id
+        };
+        const positions: GeoPosition[] = [];
+        positions.push(pos);
+        this.broker.sendMessage(
+            new ShowMapMessage(
+                positions,
+                this.userPos,
+                `${message.title} (${message.subCategory})`,
+                message.description
+            )
+        );
+    }
+    translate(key: string): string {
+        return this.translationService.translateWithArgs(key);
+    }
+    getStyleClassForPriority(prio: number): string {
+        let style = 'priority-ctn';
+        switch (prio) {
+            case 1:
+                style += ' prio-highest';
+                break;
+            case 2:
+                style += ' prio-higher';
+                break;
+            case 3:
+                style += ' prio-high';
+                break;
+            default:
+                style += ' prio-info';
+        }
+
+        return style;
+    }
+}
